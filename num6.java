89d1089a86b872fbc775c966fcc839f42adf8b93
OlovOne <104876268+OlovOne@users.noreply.github.com>
2022-05-14 19:13:44 -0300

    Rename num5.java to num6.java

--- num6.java
+++ num6.java
@@ -1,10 +1,10 @@
-package Exercicíos;
+package ExercicÃ­os;
 
 import java.util.Scanner;
 
 public class num5 {
 	public static void main(String[] args) {
-		System.out.print("Digite um número: ");
+		System.out.print("Digite um nÃºmero: ");
 
 		Scanner ler = new Scanner(System.in);
 		int n = ler.nextInt();
@@ -12,7 +12,7 @@ public class num5 {
 		int a = n - 1;
 		int s = n + 1;
 		
-		System.out.println("O antecessor desse número é " + a + " e o seu sucessor é " + s);
+		System.out.println("O antecessor desse nÃºmero Ã© " + a + " e o seu sucessor Ã© " + s);
 
 		ler.close();
 	}
