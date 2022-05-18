c9d53115954bf60b750d7b8ab5e68b660cc9f46c
OlovOne <104876268+OlovOne@users.noreply.github.com>
2022-05-17 21:23:43 -0300

    Add files via upload

---
+++ num11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class num13 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner teclado = new Scanner(System.in);{
+			System.out.println(" Quantidade de calca");
+			float camisa = teclado.nextFloat();
+			System.out.println(" Quantidade de camisa ");
+			float calca = teclado.nextFloat();
+			System.out.println(" Quantidade de meias");
+			float meia = teclado.nextFloat();
+		  System.out.println("Total a pagar: "+(15*camisa +calca*10+meia*2));
+		  
+		
+		} 
+		}
+	}
